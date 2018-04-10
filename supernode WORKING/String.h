5d20df69fb6b160ddbec68f326b59fdbcc39de6e
jedrek215 <jedrek215@gmail.com>
2018-04-11 07:30:50 +0800

    last working with ack
    last working with ack

---
+++ supernode WORKING/String.h
@@ -0,0 +1,55 @@
+const char string[] PROGMEM = R"=====(
+<html>
+<title> SEND STRING</title>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {font-family: Calibri, Helvetica, sans-serif; color:white; background:  #232428;}
+    
+form {border: 3px solid white; margin:0px 400px; background-color: white; border-radius: 10px;}
+    
+input[type=text]{
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+    border-radius: 5px;
+}
+    
+.button{
+    background-color: #296ee5;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+}
+
+.container {
+    padding: 16px;
+}
+        
+    
+@media screen and (max-width: 640px) {
+    form {border: 3px solid white; margin:0px 20px; background-color: white; border-radius: 10px;}
+}
+</style>
+</head>
+
+<body>
+<h1 align='center'>ESP 8266 Message</h1>
+      <form action ='msg'>
+          <div class='container'>
+              <label style='color:black; text-align:left;'><b>Message</b></label><br>
+              <input type='text' placeholder='Enter message'  name ='mensahe' required>
+              <br><br>
+              <input type='submit' class='button' value='Send'>
+          </div>
+      </form>
+      
+    </body>
+</html>
+)=====";
---
+++ supernode WORKING/supernode.ino
@@ -0,0 +1,88 @@
+#include <ESP8266WebServer.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMesh.h>
+
+#include "String.h"
+
+extern "C" {
+#include <user_interface.h>
+}
+bool sent=false;
+unsigned int request_i = 0;
+unsigned int response_i = 0;
+unsigned int ssid = ESP.getChipId();
+
+String stringpage = string;
+
+String manageRequest(String request);
+
+String str, decodedmsg;
+ESP8266WebServer server(80); //Server on port 80
+/* Create the mesh node object */
+ESP8266WiFiMesh mesh_node = ESP8266WiFiMesh(ssid, manageRequest);
+
+/**
+ * Callback for when other nodes send you data
+ *
+ * @request The string received from another node in the mesh
+ * @returns The string to send back to the other node
+ */
+void handleString(){
+server.send(200,"text/html",stringpage);
+
+  String str = server.arg("mensahe");
+  
+  decodedmsg = str;
+  decodedmsg.replace("+"," ");
+  
+  if(decodedmsg.length()>0)
+  {
+  Serial.print("Message:");
+  Serial.println(decodedmsg);
+  }
+  
+}
+
+String manageRequest(String request)
+{
+  /* Print out received message */
+  Serial.println("SENT!");
+  Serial.print("received: ");
+  Serial.println(request);
+  sent = true;
+  decodedmsg = "";
+  return  "";
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(10);
+  
+  Serial.println("Setting up mesh node...");
+  
+  Serial.println("HTTP server started");
+  server.on("/msg",handleString);
+  server.begin();
+  mesh_node.begin();
+}
+
+void loop()
+{
+      server.handleClient();
+      
+      if(decodedmsg.length()>0)
+      {
+        sent = false;
+        Serial.println("-------------------------------SENDING THE MESSAGE-------------------------");
+         
+         while(!sent)
+         {
+          Serial.println("Waiting to send");
+          mesh_node.attemptScan(decodedmsg);
+         }         
+      }
+}
+
+
+
